93ce1109dd9f870d05380f02c05a6bc1ce79af76
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-11 16:49:38 +1100

    Refactor

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,15 +3,12 @@ import { Box, Stack, Typography } from '@mui/material';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos } from './';
 
-
-
 const Feed = () => {
-
 	const [selectedCategory, setSelectedCategory] = useState('New');
 	const [videos, SetVideos] = useState([]);
 
 	useEffect(() => {
-		fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => SetVideos(data.items))
+		fetchFromAPI(`search?part=snippet&q=${selectedCategory}&maxResults=20`).then((data) => SetVideos(data.items))
 	}, [selectedCategory]);
 
 	return (
@@ -29,7 +26,7 @@ const Feed = () => {
 
 			<Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
 				<Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }} >
-					{selectedCategory} <span style={{ color: '#F31503'}}>videos</span>
+					{selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
 				</Typography>
 				<Videos videos={videos} />
 			</Box>
