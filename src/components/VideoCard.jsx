d114ad2695b8ce55db0f085c56612d3d19e9dba8
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-11 16:48:26 +1100

    Display number of views for each video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,30 +1,48 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
-const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
+const VideoCard = ({ video }) => {
+	const [videoDetail, setVideoDetail] = useState(null)
+
+	useEffect(() => {
+		fetchFromAPI(`videos?part=snippet,statistics&id=${video.id.videoId}&maxResults=5`)
+		.then((data) => setVideoDetail(data?.items[0]))
+	}, [video.id.videoId])
+
 	return (
 		<Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: '0' }}>
-			<Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+			<Link to={video?.id?.videoId ? `/video/${video?.id?.videoId}` : demoVideoUrl}>
 				<CardMedia 
-					image={snippet?.thumbnails?.high?.url} 
-					alt={snippet?.title}
-					sx={{ width: 358, height: 180}}
+					image={video?.snippet?.thumbnails?.high?.url} 
+					alt={video?.snippet?.title}
+					sx={{ width: 358, height: 180 }}
 					/>
 			</Link>
 			<CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
-			<Link to={videoId ? `/channel/${videoId}` : demoVideoTitle}>
-				<Typography variant="subtitle1" fontWeight="bold" color="#FFF" >
-					{snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+				<Link to={video?.id?.videoId ? `/channel/${video?.id?.videoId}` : demoVideoTitle}>
+					<Typography variant="subtitle1" fontWeight="bold" color="#FFF" >
+						{video?.snippet?.title.slice(0, 30) || demoVideoTitle.slice(0, 30)}
+					</Typography>
+				</Link>	
+				<Link to={video?.snippet?.channelId ? `/channel/${video?.snippet?.channelId}` : demoChannelUrl}>
+					<Typography variant="subtitle2" fontWeight="bold" color="gray" >
+						{video?.snippet?.channelTitle || demoChannelTitle}
+						<CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
+					</Typography>
+				</Link>
+				{videoDetail?.statistics?.viewCount && (			
+					<Typography fontSize={12} color="white">
+						{videoDetail?.statistics?.viewCount} views
+					</Typography>
+				)}
+				<Typography>
+					
 				</Typography>
-			</Link>	
-			<Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
-				<Typography variant="subtitle2" fontWeight="bold" color="gray" >
-					{snippet?.channelTitle || demoChannelTitle}
-					<CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
-				</Typography>
-			</Link>
 			</CardContent>
 		</Card>
 	)
