0a676d1384c06e273814260bdf10266613d7a807
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 00:59:13 +1100

    Add semicolon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,11 +21,11 @@ const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
 				}}
 				key={category.name}
 			>
-					<span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
-					<span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+				<span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
+				<span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
 			</button>
 		))}
 	</Stack>
 )
 
-export default Sidebar
+export default Sidebar;
