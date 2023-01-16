b7468c02d3024710f62f669e383cc176e90ad0cd
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 00:19:17 +1100

    Modify

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,27 +1,27 @@
 import { Box, CardContent, CardMedia, Typography } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
 	return (
 		<Box
-			p={2}
 			sx={{ 
 				boxShadow: 'none', 
 				borderRadius: '20px',
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
-				overflowY: 'auto',
-				width: { xs: '356px', md: '320px' },
-				height: '326px',
-				margin: 'auto'
+				width: '350px',
+				height: '320px',
+				margin: 'auto',
+				marginTop
 			}}
 		>
 		<CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#FFF' }}>
 			<CardMedia 
-				image={channelDetail?.snippet?.thumbnails?.high?.url}
+				image={channelDetail?.snippet?.thumbnails?.medium?.url}
 				alt={channelDetail?.snippet?.title}
-				sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+				sx={{ borderRadius: '50%', height: '180px', width: '180px', border: '1px solid #e3e3e3', margin:'auto'}}
+				component="img"
 			/>
 			<Typography variant="h6">
 				{channelDetail?.snippet?.title}ç
@@ -29,12 +29,12 @@ const ChannelCard = ({ channelDetail }) => {
 			</Typography>			
 				{channelDetail?.statistics?.subscriberCount && (
 					<Typography>
-						{parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+						{parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
 					</Typography>
 				)}
 				{channelDetail?.statistics?.subscriberCount && (
 					<Typography>
-						{parseInt(channelDetail?.statistics?.videoCount).toLocaleString()} Videos
+						{parseInt(channelDetail?.statistics?.videoCount).toLocaleString('en-US')} Videos
 					</Typography>
 				)}
 		</CardContent>
