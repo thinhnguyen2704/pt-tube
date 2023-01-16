5c6547de07699e6167475dfd0cfff1d3d23a2c51
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-16 16:15:58 +1100

    Add app name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { logo } from '../utils/constants';
@@ -9,10 +9,11 @@ const Navbar = () => (
 		direction="row" 
 		alignItems="center" 
 		p={2} 
-		sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between"}}
+		sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between" }}
 	>
 		<Link to="/" style={{ display: "flex", alignItems: "center" }}>
-			<img src={logo} alt="logo" height={45} />
+			<img src={logo} alt="logo" height={45} /> 
+			<Typography variant="h4" color="#fff">PTube</Typography>
 		</Link>
 		<SearchBar />
 	</Stack>
