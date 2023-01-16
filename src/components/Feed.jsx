b42bd6c107ca4226976e8d07692f076e9cee4b89
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 01:00:18 +1100

    Code refactor

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,34 +4,32 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos } from './';
 
 const Feed = () => {
-	const [selectedCategory, setSelectedCategory] = useState('New');
+	const [selectedCategory, setSelectedCategory] = useState('Latest');
 	const [videos, SetVideos] = useState([]);
 
 	useEffect(() => {
-		fetchFromAPI(`search?part=snippet&q=${selectedCategory}&maxResults=20`).then((data) => SetVideos(data.items))
+		fetchFromAPI(`search?part=snippet,id&q=${selectedCategory}&maxResults=40`).then((data) => SetVideos(data.items));
 	}, [selectedCategory]);
 
 	return (
-		<Stack sx={{ flexDirection: { sx: "column", md: "row"} }}>
+		<Stack sx={{ flexDirection:{ sx: "column", md: "row" } }}>
 			<Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
 				<Sidebar 
 					selectedCategory={selectedCategory}
 					setSelectedCategory={setSelectedCategory}
 				/>
-
 				<Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}}>
 					Copyright 2023 PT Entertainment
 				</Typography>
 			</Box>
-
 			<Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-				<Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }} >
+				<Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
 					{selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
 				</Typography>
-				<Videos videos={videos} />
+				<Videos videos={videos}/>
 			</Box>
 		</Stack>
 	)
 }
 
-export default Feed
+export default Feed;
