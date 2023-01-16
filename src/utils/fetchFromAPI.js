1ce13998bd4fee0a2d33c9ba991ba89bdc9cf545
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-16 16:12:47 +1100

    Remove maxResults from params

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,7 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   url: BASE_URL,
-  params: {maxResults: '50'
+  params: {
 	},
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
@@ -14,7 +14,7 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
 	
-	// Destructure response to only get data
+	// Destructure response to only get the data
 	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
 	return data;
