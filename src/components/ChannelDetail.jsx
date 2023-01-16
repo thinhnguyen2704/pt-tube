899f8af81ebd676a27a20b0795bb12bf4b15694a
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 00:19:08 +1100

    Modify

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,38 +1,48 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 
 import { Videos, ChannelCard } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-	const [channelDetail, setChannelDetail] = useState(null)
-	const [videos, setVideos] = useState([])
+	const [videos, setVideos] = useState([]);
+	const [channelDetail, setChannelDetail] = useState('null');
 
 	const { id } = useParams();
 
-	useEffect(() => { 
-		fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+	useEffect(() => {
+		const fetchResults = async () => {
+			const channelVideos = await fetchFromAPI(`search?channelId=${id}&part=snippet&order=date&maxResults=40`);
+			setVideos(channelVideos?.items);
 
-		fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items))
+			const channelData = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+			setChannelDetail(channelData.items[0]);
+		}
 
-	}, [id])
+		fetchResults();
+	}, [id]);
+	
+	if(!channelDetail) return 'Loading channel';
+
 	return (
-		<Box minHeight="95vh">
+		<Stack flexDirection="column">
 			<Box>
-				<div style={{
-					background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 45%, rgba(0,212,255,1) 100%)',
-					zIndex: 10,
-					height: '300px'
-				}} />
-				<ChannelCard channelDetail={channelDetail} marginTop="-110px" />
-			<Box display="flex" p="2">
-				<Box sx={{ mr: { sm: '100px' } }}>
-					<Videos videos={videos} />
-				</Box>
-			</Box>	
+				<Box>
+					<div style={{
+						background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 45%, rgba(0,212,255,1) 100%)',
+						zIndex: 10,
+						height: '300px'
+					}} />
+					<ChannelCard channelDetail={channelDetail}/>
+				</Box>	
+				<Box sx={{ margin: 'auto' }}>
+					<Box>
+						<Videos videos={videos} />
+					</Box>
+				</Box>	
 			</Box>
-		</Box>
+		</Stack>
 	)
 }
 
