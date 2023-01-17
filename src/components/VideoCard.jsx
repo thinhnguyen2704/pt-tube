f3300cb516cdda5f561225ddcec628fd6513f095
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 23:11:51 +1100

    Destructure video

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,27 +2,29 @@ import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-const VideoCard = ({ video }) => {
+const VideoCard = (video) => {
+	const { video: { id: { videoId }, snippet: { thumbnails: { medium: { url } }, title, channelId, channelTitle } } } = video;
+	
 	return (
 		<Card sx={{ width: { xs: '100%', sm:'175px', md: '250px' }, boxShadow: 'none', borderRadius: '5%', margin:'auto' }}>
-			<Link to={`/video/${video?.id?.videoId}`}>
-				<CardMedia className="video-img"
-					image={video?.snippet?.thumbnails?.medium?.url}
-					alt={video?.snippet?.title}
+			<Link to={`/video/${videoId}`}>
+				<CardMedia
+					image={url}
+					alt={title}
 					sx={{ width: { xs: '100%', sm:'175px', md: '250px' }, height:'160px' }}
 					component="img"
-					/>
+				/>
 			</Link>
 			<CardContent sx={{ backgroundColor: '#1e1e1e', height: '60px' }}>
 				<Stack direction="column">
-					<Link to={`/video/${video?.id?.videoId}`}>
+					<Link to={`/video/${videoId}`}>
 						<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
-							{video?.snippet?.title.slice(0, 40)}
+							{title.slice(0, 40)}
 						</Typography>
 					</Link>	
-					<Link to={`/channel/${video?.snippet?.channelId}`}>
+					<Link to={`/channel/${channelId}`}>
 						<Typography variant="subtitle2" fontWeight="bold" color="gray">
-							{video?.snippet?.channelTitle}
+							{channelTitle}
 							<CheckCircle sx={{ fontSize: 11, color: 'gray', ml: '5px' }}/>
 						</Typography>
 					</Link>
