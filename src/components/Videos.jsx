bab8893ffe1d56187c546c2a2fdbbb382d3b1a33
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 23:12:51 +1100

    Modify tag name

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,14 +3,13 @@ import { PlaylistCard, VideoCard } from './';
 
 const Videos = ({ videos, direction }) => {
 	if (!videos?.length) return 'Loading...';
-	console.log(videos)
 
 	return (
 		<Stack direction= {direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
 			{videos.map((item, idx) => (
 				<Box key={idx}>
 					{item.id.videoId && <VideoCard video={item} />}
-					{item.id.playlistId && <PlaylistCard  playlistVideo={item}/>}
+					{item.id.playlistId && <PlaylistCard  playlist={item}/>}
 				</Box>
 			))}
 		</Stack>
