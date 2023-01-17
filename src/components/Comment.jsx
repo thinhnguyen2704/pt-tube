38a46649292fe2fec2e57120d5e4ab13cb5e924e
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 19:24:37 +1100

    Show elapsedTime and fix channelId

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,33 @@
 import { Box, CardMedia, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+const calculateElapsedTime = (publishedTime) => {
+	var elapsedTime = new Date() - Date.parse(publishedTime);
+
+	if(elapsedTime >= 31557600000) {
+		elapsedTime = Math.floor(elapsedTime/31557600000).toLocaleString('en-US');
+		return (elapsedTime >= 2) ? elapsedTime += " years ago" : elapsedTime += " year ago";
+	} else if(elapsedTime >= 86400000) {
+		elapsedTime = Math.floor(elapsedTime/86400000).toLocaleString('en-US');
+		return (elapsedTime >= 2) ? elapsedTime += " months ago" : elapsedTime += " month ago";
+	} else if(elapsedTime >= 3600000) {
+		elapsedTime = Math.floor(elapsedTime/3600000).toLocaleString('en-US');
+		return (elapsedTime >= 2) ? elapsedTime += " hours ago" : elapsedTime += " hour ago";
+	} else {
+		elapsedTime = Math.floor(elapsedTime/60000).toLocaleString('en-US');
+		return (elapsedTime >= 2) ? elapsedTime += " minutes ago" : elapsedTime += " minute ago";
+	}
+}
+
 const Comment = (comment) => {
 	if (!comment) return 'Loading comments';
+
+	const elapsedTime = calculateElapsedTime(comment?.comment?.snippet?.topLevelComment?.snippet?.publishedAt);
 
 	return (
 		<Box>
 			<Stack direction="row" gap={1.5}>
-				<Link to={`/channel/${comment?.comment?.snippet?.channelId}`}>
+				<Link to={`/channel/${comment?.comment?.snippet?.topLevelComment?.snippet?.authorChannelId?.value}`}>
 					<CardMedia
 						image={comment?.comment?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl}
 						sx={{ borderRadius: '50%', height: '40px', width: '40px', border: '1px solid #e3e3e3', margin:'auto'}}
@@ -15,13 +35,18 @@ const Comment = (comment) => {
 					/>
 				</Link>
 				<Stack direction="column">
-					<Link to={`/channel/${comment?.comment?.snippet?.channelId}`}>
-						<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
-							{comment?.comment?.snippet?.topLevelComment?.snippet?.authorDisplayName}
+					<Stack direction="row" gap={1.5}>
+						<Link to={`/channel/${comment?.comment?.snippet?.topLevelComment?.snippet?.authorChannelId?.value}`}>
+							<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
+								{comment?.comment?.snippet?.topLevelComment?.snippet?.authorDisplayName}
+							</Typography>
+						</Link>
+						<Typography variant="body2" color="gray">
+							{elapsedTime}
 						</Typography>
-					</Link>
+					</Stack>
 					<Typography variant="body2" color="#fff">
-						{comment?.comment?.snippet?.topLevelComment?.snippet?.textDisplay}
+						{comment?.comment?.snippet?.topLevelComment?.snippet?.textDisplay.toLocaleString('en-US')}
 					</Typography>
 				</Stack>
 			</Stack>
