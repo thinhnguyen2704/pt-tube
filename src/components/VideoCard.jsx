063d7ba3b5056e0ae2c1689252c1764972535eb1
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-17 14:30:48 +1100

    Modfiy VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,10 @@
-// import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { Typography, Card, CardContent, CardMedia, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 const VideoCard = ({ video }) => {
 	return (
-		<Card sx={{ width: { xs: '100%', sm:'175px', md: '250px' }, boxShadow: 'none', borderRadius: '0', margin:'auto' }}>
+		<Card sx={{ width: { xs: '100%', sm:'175px', md: '250px' }, boxShadow: 'none', borderRadius: '5%', margin:'auto' }}>
 			<Link to={`/video/${video?.id?.videoId}`}>
 				<CardMedia className="video-img"
 					image={video?.snippet?.thumbnails?.medium?.url}
@@ -15,17 +14,19 @@ const VideoCard = ({ video }) => {
 					/>
 			</Link>
 			<CardContent sx={{ backgroundColor: '#1e1e1e', height: '60px' }}>
-				<Link to={`/video/${video?.id?.videoId}`}>
-					<Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-						{video?.snippet?.title.slice(0, 30)}
-					</Typography>
-				</Link>	
-				<Link to={`/channel/${video?.snippet?.channelId}`}>
-					<Typography variant="subtitle2" fontWeight="bold" color="gray">
-						{video?.snippet?.channelTitle}
-						<CheckCircle sx={{ fontSize: 11, color: 'gray', ml: '5px' }}/>
-					</Typography>
-				</Link>
+				<Stack direction="column">
+					<Link to={`/video/${video?.id?.videoId}`}>
+						<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
+							{video?.snippet?.title.slice(0, 40)}
+						</Typography>
+					</Link>	
+					<Link to={`/channel/${video?.snippet?.channelId}`}>
+						<Typography variant="subtitle3" fontWeight="bold" color="gray">
+							{video?.snippet?.channelTitle}
+							<CheckCircle sx={{ fontSize: 11, color: 'gray', ml: '5px' }}/>
+						</Typography>
+					</Link>
+				</Stack>
 			</CardContent>
 		</Card>
 	)
