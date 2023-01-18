8ea6bf5f37b9a86513304de0c0aaeb1cec988958
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-18 17:23:54 +1100

    Add zIndex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => (
 		direction="row" 
 		alignItems="center" 
 		p={2} 
-		sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between" }}
+		sx={{ position: "sticky", background: "#000", top: 0, justifyContent: "space-between", zIndex: "1000" }}
 	>
 		<Link to="/" style={{ display: "flex", alignItems: "center" }}>
 			<img src="https://cdn2.iconfinder.com/data/icons/social-aquiocons/512/Aquicon-Youtube.png" alt="logo" height={45} /> 
