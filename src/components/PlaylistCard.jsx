8a315b0e2da4cef3368ff77eda79f069f259ca29
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-20 23:39:45 +1100

    Modify link

--- src/components/PlaylistCard.jsx
+++ src/components/PlaylistCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia, Stack, Box } from '@mui/material';
+import { Typography, Card, CardContent, CardMedia, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
 const PlaylistCard = (playlist) => {
@@ -8,7 +8,7 @@ const PlaylistCard = (playlist) => {
 
 	return (
 		<Card sx={{ width: { xs: "100%", sm:"175px", md: "250px" }, boxShadow: "none", borderRadius: "5%", margin:"auto" }}>
-			<Link to={`/video/${playlistId}`}>
+			<Link to={`/playlist/${playlistId}`}>
 				<CardMedia className="video-img"
 					image={url}
 					alt={title}
@@ -18,9 +18,9 @@ const PlaylistCard = (playlist) => {
 			</Link>
 			<CardContent sx={{ backgroundColor: "#1e1e1e", height: "60px" }}>
 				<Stack direction="column">
-					<Link to={`/video/${playlistId}`}>
+					<Link to={`/playlist/${playlistId}`}>
 						<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
-							{title.slice(0, 40)}
+							<div dangerouslySetInnerHTML={{__html: title.slice(0, 40)}} />
 						</Typography>
 					</Link>	
 					<Link to={`/channel/${channelId}`}>
