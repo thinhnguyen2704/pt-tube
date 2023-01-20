f96527411100f0105d25fd0542b9ca5af25da33c
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-20 23:32:12 +1100

    Add PlaylistVideos annd PlaylistDetail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+import { Navbar, Feed, VideoDetail, ChannelDetail, PlaylistVideos, PlaylistDetail, SearchFeed} from './components';
 
 const App = () => (
 	<BrowserRouter>
@@ -11,6 +11,8 @@ const App = () => (
 				<Route path="/" exact element={<Feed />} />
 				<Route path="/video/:id" element={<VideoDetail />} />
 				<Route path="/channel/:id" element={<ChannelDetail />} />
+				<Route path="/playlist/:id" element={<PlaylistVideos />} />
+				<Route path="/video/:id&list=:id" elemetn={<PlaylistDetail />} />
 				<Route path="/search/:searchTerm" element={<SearchFeed />} />
 			</Routes>
 		</Box>
