79d297922005567bd953780ac22c6c4c63e7266d
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-24 23:43:06 +1100

    Show video published time

--- src/components/PlaylistCard.jsx
+++ src/components/PlaylistCard.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
+import calculateElapsedTime from '../utils/calculateElapsedTime';
 
 const PlaylistCard = (playlist) => {
-	
-	const { playlist: { id: { playlistId }, snippet: { thumbnails: { medium: { url } }, title, channelId, channelTitle } } } = playlist;
+	const { playlist: { id: { playlistId }, snippet: { thumbnails: { medium: { url } }, title, channelId, channelTitle, publishedAt } } } = playlist;
+
+	const elapsedTime = calculateElapsedTime(publishedAt);
 
 	return (
 		<Card sx={{ width: { xs: "100%", sm:"175px", md: "250px" }, boxShadow: "none", borderRadius: "5%", margin:"auto" }}>
@@ -16,7 +18,7 @@ const PlaylistCard = (playlist) => {
 					component="img"
 					/>
 			</Link>
-			<CardContent sx={{ backgroundColor: "#1e1e1e", height: "60px" }}>
+			<CardContent sx={{ backgroundColor: "#1e1e1e", height: "70px" }}>
 				<Stack direction="column">
 					<Link to={`/playlist/${playlistId}`}>
 						<Typography variant="subtitle2" fontWeight="bold" color="#FFF">
@@ -29,6 +31,9 @@ const PlaylistCard = (playlist) => {
 							<CheckCircle sx={{ fontSize: 11, color: "gray", ml: "5px" }}/>
 						</Typography>
 					</Link>
+					<Typography variant="subtitle2" color="gray">
+						{elapsedTime}
+					</Typography>
 				</Stack>
 			</CardContent>
 		</Card>
