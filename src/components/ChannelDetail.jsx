e43397583d83a844a8058c5f2715151a7233b0b5
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-27 00:28:14 +1100

    Add channel banner image

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Stack } from '@mui/material';
+import { Box, CardMedia, Stack } from '@mui/material';
 
 import { Videos, ChannelCard } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
@@ -24,22 +24,19 @@ const ChannelDetail = () => {
 	}, [id]);
 	
 	if(!channelDetail) return 'Loading channel';
+	document.title = channelDetail?.snippet?.title;
 
 	return (
 		<Stack flexDirection="column">
 			<Box>
-				<Box>
-					<div style={{
-						background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 45%, rgba(0,212,255,1) 100%)",
-						zIndex: 10,
-						height: "300px"
-					}} />
-					<ChannelCard channelDetail={channelDetail}/>
-				</Box>	
+				<CardMedia
+					image={channelDetail?.brandingSettings?.image?.bannerExternalUrl}
+					sx={{ maxHeight: "300px" }}
+					component="img"
+				/>
+				<ChannelCard channelDetail={channelDetail}/>
 				<Box sx={{ justifyContent:"center", alignItems:"center", marginLeft:"2%" }}>
-					<Box>
-						<Videos videos={videos} />
-					</Box>
+					<Videos videos={videos} />
 				</Box>	
 			</Box>
 		</Stack>
