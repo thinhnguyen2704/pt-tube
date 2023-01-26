24de8367707162ac5fd205d9dd0a6db19ecd7b62
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-27 02:36:39 +1100

    Remove categories

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,13 +2,12 @@ import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { fetchFromAPI } from '../utils/fetchFromAPI';
-import { Sidebar, Videos } from './';
+import { Videos } from './';
 
 import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('Home');
   const { searchTerm } = useParams();
 
   useEffect(() => {
@@ -16,12 +15,6 @@ const SearchFeed = () => {
       (data) => setVideos(data.items)
     );
   }, [searchTerm]);
-
-  useEffect(() => {
-    fetchFromAPI(
-      `search?part=snippet&q=${selectedCategory}&maxResults=20`
-    ).then((data) => setVideos(data.items));
-  }, [selectedCategory]);
 
   document.title = searchTerm;
 
@@ -33,19 +26,7 @@ const SearchFeed = () => {
           borderRight: '1px solid #3d3d3d',
           px: { sx: 0, md: 2 },
         }}
-      >
-        <Sidebar
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
-        <Typography
-          className='copyright'
-          variant='body2'
-          sx={{ mt: 1.5, color: '#fff' }}
-        >
-          Copyright 2023 PT Entertainment
-        </Typography>
-      </Box>
+      ></Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography
           variant='h4'
