33f7d9ba98ca90350ef160992e930d766cfd04ca
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-27 00:28:01 +1100

    Modify JSON structure properties and styles

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,45 +1,38 @@
-import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { Avatar, Box, CardContent, Typography } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-const ChannelCard = (channelDetail) => {
-	console.log(channelDetail)
+const ChannelCard = (channelDetail) => {			
+
 	return (
-		<Box
+		<Box		
 			sx={{ 
-				boxShadow: "none", 
-				borderRadius: "20px",
+				boxShadow: "none",
 				display: "flex",
 				justifyContent: "center",
-				alignItems: "center",
-				width: "350px",
-				height: "320px",
-				margin: "auto",
-				left: "0",
-				right: "0"
+				alignItems: "center"
 			}}
-	>
-		<CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "center", color: "#fff" }}>
-			<CardMedia 
-				image={channelDetail?.snippet?.thumbnails?.medium?.url}
-				alt={channelDetail?.snippet?.title}
-				sx={{ borderRadius: "50%", height: "180px", width: "180px", border: "1px solid #e3e3e3"}}
-				component="img"
-			/>
-			<Typography variant="h6">
-				{channelDetail?.snippet?.title}
-				<CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
-			</Typography>
-				{channelDetail?.statistics?.subscriberCount && (
+		>				
+			<CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems:"center", textAlign: "center", color: "#fff" }}>
+				<Avatar 
+					src={channelDetail?.channelDetail?.snippet?.thumbnails?.medium?.url}
+					alt={channelDetail?.channelDetail?.snippet?.title}
+					sx={{ borderRadius: "50%", height: "180px", width: "180px", border: "1px solid #e3e3e3" }}
+				/>
+				<Typography variant="h6">
+					{channelDetail?.channelDetail?.snippet?.title}
+					<CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+				</Typography>
+				{channelDetail?.channelDetail?.statistics?.subscriberCount && (
 					<Typography>
-						{parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
+						{parseInt(channelDetail?.channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
 					</Typography>
 				)}
-				{channelDetail?.statistics?.videoCount && (
+				{channelDetail?.channelDetail?.statistics?.videoCount && (
 					<Typography>
-						{parseInt(channelDetail?.statistics?.videoCount).toLocaleString('en-US')} Videos
+						{parseInt(channelDetail?.channelDetail?.statistics?.videoCount).toLocaleString('en-US')} Videos
 					</Typography>
 				)}
-		</CardContent>
-	</Box>
+			</CardContent>
+		</Box>
 )}
 export default ChannelCard;
