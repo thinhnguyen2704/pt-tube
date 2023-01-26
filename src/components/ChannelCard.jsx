c61f7ff2e163eec610228acb7da7b125e9c8b0cd
Nguyen Duc Thinh <thinh.9a4.dt@gmail.com>
2023-01-27 02:36:59 +1100

    Modify component styles

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -7,50 +7,52 @@ const ChannelCard = (channelDetail) => {
       sx={{
         boxShadow: 'none',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
       }}
     >
       <CardContent
         sx={{
           display: 'flex',
-          flexDirection: 'column',
+          flexDirection: 'row',
           justifyContent: 'center',
           alignItems: 'center',
           textAlign: 'center',
           color: '#fff',
         }}
       >
-        <Avatar
-          src={channelDetail?.channelDetail?.snippet?.thumbnails?.medium?.url}
-          alt={channelDetail?.channelDetail?.snippet?.title}
-          sx={{
-            borderRadius: '50%',
-            height: '180px',
-            width: '180px',
-            border: '1px solid #e3e3e3',
-          }}
-        />
-        <Typography variant='h6'>
-          {channelDetail?.channelDetail?.snippet?.title}
-          <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
-        </Typography>
-        {channelDetail?.channelDetail?.statistics?.subscriberCount && (
-          <Typography>
-            {parseInt(
-              channelDetail?.channelDetail?.statistics?.subscriberCount
-            ).toLocaleString('en-US')}{' '}
-            Subscribers
+				<Box  sx={{ direction: 'column'}}>
+					<Avatar
+						src={channelDetail?.channelDetail?.snippet?.thumbnails?.medium?.url}
+						alt={channelDetail?.channelDetail?.snippet?.title}
+						sx={{
+							borderRadius: '50%',
+							height: { xs: '100px', md: '180px' },
+							width: { xs: '100px', md: '180px' },
+							border: '1px solid #e3e3e3',
+						}}
+					/>
+          <Typography variant='h6'>
+            {channelDetail?.channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
           </Typography>
-        )}
-        {channelDetail?.channelDetail?.statistics?.videoCount && (
-          <Typography>
-            {parseInt(
-              channelDetail?.channelDetail?.statistics?.videoCount
-            ).toLocaleString('en-US')}{' '}
-            Videos
-          </Typography>
-        )}
+				</Box>
+        <Box sx={{ direction: 'column', ml: '20px' }}>
+          {channelDetail?.channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.channelDetail?.statistics?.subscriberCount
+              ).toLocaleString('en-US')}{' '}
+              Subscribers
+            </Typography>
+          )}
+          {channelDetail?.channelDetail?.statistics?.videoCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.channelDetail?.statistics?.videoCount
+              ).toLocaleString('en-US')}{' '}
+              Videos
+            </Typography>
+          )}
+        </Box>
       </CardContent>
     </Box>
   );
